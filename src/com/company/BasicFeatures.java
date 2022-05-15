d052dd558ddb9cd335f30b6636a5fd010ded618e
RazielEL <amp91@pm.me>
2022-05-15 11:04:52 -0500

    the basic structure of the car is ready, working on advance features

---
+++ src/com/company/BasicFeatures.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class BasicFeatures {
+
+    String engineCapac;
+    String color;
+    String doors;
+    String headlights;
+    String regNo;
+
+    public static void main (String[] args){
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	System.out.println("Stashing Project");
+
+    for (int i = 0; i < 100; i++){
+        System.out.print(i + " ");
+    }
+
+    BasicFeatures car = new BasicFeatures();
+    
+
+
+
+    }
+}
